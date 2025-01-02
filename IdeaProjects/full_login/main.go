18216c6114d8bbd6412b6114e616a999719f87e6
kdw240 <kdw240@gmail.com>
2025-01-03 07:25:30 +0900

    api test

---
+++ IdeaProjects/full_login/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"full_login/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	public := r.Group("/api")
+
+	public.POST("/register", controllers.Register)
+
+	r.Run(":8080")
+
+}
