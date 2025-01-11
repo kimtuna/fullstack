5bc7b7f88634342ab0a8344ae90b20537bc68936
kdw240 <kdw240@gmail.com>
2025-01-11 12:55:12 +0900

    call? 된건가 모르겠네 이해가 안되긴하는데

--- IdeaProjects/full_call/main.go
+++ IdeaProjects/full_call/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
-	"full_call/controllers"
-	"full_call/middlewares"
-	"full_call/models"
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
+  "github.com/gorilla/websocket"
+  "log"
 )
 
+var upgrader = websocket.Upgrader{}
+
 func main() {
-
-	models.ConnectDataBase()
-
 	r := gin.Default()
 
-	// 방 생성 및 참가/퇴장 라우터 설정
-	protected := r.Group("/api")
-	protected.Use(middlewares.JwtAuthMiddleware()) // JWT 인증 미들웨어 사용
-	protected.POST("/rooms", controllers.CreateRoom)
+	r.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println("Error during connection upgrade:", err)
+			return
+		}
+		defer conn.Close()
 
-	r.Run(":8081") // 서버 실행
+		for {
+			messageType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Error reading message:", err)
+				break
+			}
+
+			// 모든 클라이언트에 메시지 전송
+			// 이 부분에서 신호 처리를 구현
+			err = conn.WriteMessage(messageType, msg)
+			if err != nil {
+				log.Println("Error writing message:", err)
+				break
+			}
+		}
+	})
+
+	r.Run(":8080") // 서버 실행
 }
--- IdeaProjects/full_call/models/room.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Room struct {
-	ID       string `gorm:"primaryKey"`
-	ParentID uint   // 사용자 UID
-	Name     string // 방 이름
-}
